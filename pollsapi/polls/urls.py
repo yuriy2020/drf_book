afbc533ee9269084148a1f0fde9cf47523c9b379
yuri <ybalalaev@gmail.com>
2024-06-10 22:08:14 +0300

    api только джанго

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+from .views import poll_list, polls_detail
+
 urlpatterns = [
+    path("polls/", poll_list, name="polls_list"),
+    path("polls/<int:pk>/", polls_detail, name="polls_detail")
 ]
--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .models import Poll, Choice
+
 
 # Create your views here.
+def poll_list(request):
+    MAX_OBJECTS = 20
+    polls = Poll.objects.all()[:MAX_OBJECTS]
+    data = {
+        "results": list(polls.values("questions", "created_by__username", "pub_date"))
+    }
+    return JsonResponse(data)
+
+
+def polls_detail(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    data = {
+        "results": poll.questions,
+        "created_by": poll.created_by.username,
+        "pub_date": poll.pub_date
+    }
+    return JsonResponse(data)
