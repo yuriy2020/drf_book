6282c68f41b7196d20c06beaa2f068d6c951b157
yuri <ybalalaev@gmail.com>
2024-06-13 00:15:32 +0300

    users

--- pollsapi/polls/apiviews.py
+++ pollsapi/polls/apiviews.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status, viewsets
 from .models import Poll, Choice
-from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
 
 
 # class PollList(generics.ListCreateAPIView):
@@ -43,3 +43,7 @@ class CreateVote(APIView):
             return Response(my_serializer.data, status.HTTP_201_CREATED)
         else:
             return Response(my_serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class UserCreate(generics.CreateAPIView):
+    serializer_class = UserSerializer
