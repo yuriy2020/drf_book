471c7934169430d5d60171750a853383179f7cba
yuri <ybalalaev@gmail.com>
2024-06-12 12:46:10 +0300

    psql db

--- pollsapi/pollsapi/settings.py
+++ pollsapi/pollsapi/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'pollsapi.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drf_001',  # Replace xxx with your database name
+        'USER': 'postgres',  # Replace xxx with your database user
+        'PASSWORD': '123456qQ',  # Replace xxx with your database password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
